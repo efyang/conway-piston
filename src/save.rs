63cbcaf1657421ff346ff477d8ee89f0e004efa4
Edward Yang <edward.yang6771@gmail.com>
2015-09-07 14:31:10 -0500

    More work on saving

--- src/save.rs
+++ src/save.rs
@@ -2,6 +2,11 @@ pub fn parse(filepath: &str) -> Vec<Vec<bool>> {
     unimplemented!();
 }
 
-pub fn save(filepath: &str) {
+pub fn save(filepath: &str, data: Vec<Vec<bool>>) {
+    //should be 
+    unimplemented!();
+}
+
+pub fn clear_saves(savedir: &str) {
     unimplemented!();
 }
