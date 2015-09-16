6a114d55426db4343543b84a11479b97ec1ae97a
Edward Yang <edward.yang6771@gmail.com>
2015-09-16 18:38:23 -0500

    Fully implemented colors

--- src/hex.rs
+++ src/hex.rs
@@ -1,16 +1,15 @@
 pub fn validate_hex(hexstr: &str) -> Option<&str> {
     let inputlength: usize = hexstr.len();
-    if inputlength != 7 {
+    if inputlength != 6 {
         None
     }
     else{
         let hexchars = hexstr.chars();
         let revhexchars = hexchars.rev();
-        if hexstr.chars().collect::<Vec<char>>()[0] == '#' && revhexchars
-            .take(inputlength - 1)
+        if revhexchars
             .filter(|x| x.is_alphanumeric())
             .collect::<String>()
-            .len() == inputlength - 1 {
+            .len() == inputlength {
             Some(hexstr)
         }
         else {
